a187fa456acaa85c7cc17e4df58d034a57793e77
Richard Duah Boakye <richrichie784@gmail.com>
2024-03-21 18:19:41 +0000

    Create db_storage.py

---
+++ models/engine/db_storage.py
@@ -0,0 +1,28 @@
+
+#!/usr/bin/python3
+""" DBStorage Module"""
+from sqlalchemy import create_engine
+import os
+
+
+dialect = 'mysql'
+driver = 'mysqldb'
+HBNB_MYSQL_USER = os.getenv('HBNB_MYSQL_USER')
+HBNB_MYSQL_PWD = os.getenv('HBNB_MYSQL_PWD')
+HBNB_MYSQL_HOST = os.getenv('HBNB_MYSQL_HOST')
+HBNB_MYSQL_DB = os.getenv('HBNB_MYSQL_DB')
+HBNB_ENV = os.getenv('HBNB_ENV')
+
+
+class DBStorage:
+    """ The city class, contains state ID and name """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine = create_engine(f"{dialect}+{driver}://\
+            {HBNB_MYSQL_USER}:{HBNB_MYSQL_PWD}@\
+            {HBNB_MYSQL_HOST}/{HBNB_MYSQL_DB}", pool_pre_ping=True)
+        if HBNB_ENV == 'test':
+            """TODO: Drop all tables"""
+            pass
