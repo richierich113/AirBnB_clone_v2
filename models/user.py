b322085796c453e880bbdb8cbc3631175daaeb98
Richard Duah Boakye <richrichie784@gmail.com>
2024-03-21 19:13:13 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -11,5 +11,5 @@ class User(BaseModel, Base):
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     # can be null
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
