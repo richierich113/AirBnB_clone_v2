b276419b0fabf4ad3da0ef0b2d0dd200bbefac6d
Richard Duah Boakye <richrichie784@gmail.com>
2024-03-23 23:15:56 +0000

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-""" """
+""" review module testing"""
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ """
+    """ class for review module testing """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ init function to initialize args """
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """ place id test function"""
         new = self.value()
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """ user_id test function """
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
+        """ text test function"""
         new = self.value()
         self.assertEqual(type(new.text), str)
