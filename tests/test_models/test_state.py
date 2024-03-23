392625e5acef031ece03f8bf654645283c9d4e00
Richard Duah Boakye <richrichie784@gmail.com>
2024-03-23 23:17:34 +0000

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+""" state module testing """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """ state module testing class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ init function """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
-    def test_name3(self):
-        """ """
+    def test_name(self):
+        """ name test function"""
         new = self.value()
         self.assertEqual(type(new.name), str)
