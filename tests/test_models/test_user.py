ca146e2678f4bbef45643fbf057efa53b5925a91
Richard Duah Boakye <richrichie784@gmail.com>
2024-03-23 23:25:11 +0000

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
-""" """
+""" user module testing"""
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
 
 
 class test_User(test_basemodel):
-    """ """
+    """ class for testing user module """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ init function"""
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """ """
+        """ first name test function"""
         new = self.value()
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
-        """ """
+        """ last name test function """
         new = self.value()
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
-        """ """
+        """ email test function"""
         new = self.value()
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
-        """ """
+        """ password test function
+        """
         new = self.value()
         self.assertEqual(type(new.password), str)
