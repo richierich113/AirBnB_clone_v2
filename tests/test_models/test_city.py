249dbe0a0d383aefa0af6f3869affff3a52b60a7
Richard Duah Boakye <richrichie784@gmail.com>
2024-03-23 22:55:51 +0000

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" """
+""" city module testing """
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
 
 class test_City(test_basemodel):
-    """ """
+    """ test class for city module """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ initialize args """
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
