a4d27e30e08eb07f6aa2000807aa5d304e94b810
Richard Duah Boakye <richrichie784@gmail.com>
2024-03-24 05:07:32 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,3 +1,4 @@
+'''
 #!/usr/bin/python3
 """ State Module """
 from models.base_model import BaseModel, Base
@@ -8,3 +9,34 @@ class State(BaseModeli, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+'''
+#!/usr/bin/python3
+"""This is the state class"""
+import models
+from models.city import City
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import environ
+
+
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City",
+                          backref="state",
+                          cascade="all, delete-orphan",
+                          passive_deletes=True)
+
+    
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            return [city for city in models.storage.all(
+                City).values() if city.state_id == self.id]
+
