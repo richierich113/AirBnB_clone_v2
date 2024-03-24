99255a2920381e7961eff480107caa6806a77c68
Richard Duah Boakye <richrichie784@gmail.com>
2024-03-24 05:03:17 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,3 +1,4 @@
+'''
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
@@ -13,3 +14,34 @@ class User(BaseModel, Base):
     # can be null
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    '''
+#!/usr/bin/python3
+"""This is the user class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+        places: places associated with the User
+        reviews: reviews the User has made
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place',
+                          backref='user',
+                          cascade='all, delete-orphan',
+                          passive_deletes=True)
+    reviews = relationship('Review',
+                           backref='user',
+                           cascade='all, delete-orphan',
+                           passive_deletes=True)
