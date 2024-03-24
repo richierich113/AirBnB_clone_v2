ae4e8ebf4c9b515a489b82f7374dca2fb22aeae2
Richard Duah Boakye <richrichie784@gmail.com>
2024-03-24 05:13:47 +0000

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,3 +1,4 @@
+'''
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
@@ -5,3 +6,18 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     name = ""
+'''
+#!/usr/bin/python3
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
