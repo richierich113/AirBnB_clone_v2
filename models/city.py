cbf38bf77cf5e9045c90211bd23dd05289dd1f2e
Richard Duah Boakye <richrichie784@gmail.com>
2024-03-24 05:08:38 +0000

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,3 +1,4 @@
+'''
 #!/usr/bin/python3
 """ City Module """
 from models.base_model import BaseModel, Base
@@ -10,3 +11,27 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    '''
+#!/usr/bin/python3
+"""This is the city class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(BaseModel, Base):
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60),
+                      ForeignKey("states.id",
+                                 ondelete='CASCADE'),
+                      nullable=False)
+    places = relationship(
+        'models.place.Place',
+        backref='cities',
+        cascade='all, delete-orphan')
