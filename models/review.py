8ff184f0f634510c2aa9bbb090387136b34805e1
Richard Duah Boakye <richrichie784@gmail.com>
2024-03-24 05:05:59 +0000

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,3 +1,4 @@
+'''
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
@@ -11,3 +12,27 @@ class Review(BaseModel, Base):
     text = Column(String(1024), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    '''
+#!/usr/bin/python3
+"""This is the review class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String
+
+
+class Review(BaseModel, Base):
+    """This is the class for Review
+    Attributes:
+        place_id: place id
+        user_id: user id
+        text: review description
+    """
+    __tablename__ = "reviews"
+    place_id = Column(String(60),
+                      ForeignKey('places.id',
+                                 ondelete='CASCADE'),
+                      nullable=False)
+    user_id = Column(String(60),
+                     ForeignKey('users.id',
+                                ondelete='CASCADE'),
+                     nullable=False)
+    text = Column(String(1024), nullable=False)
