c6345a2ab511d04719717ba617d6b4861e260315
Richard Duah Boakye <richrichie784@gmail.com>
2024-04-25 01:23:35 +0000

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,18 +17,22 @@ from urllib.parse import unquote
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def display_c_text(text):
     text = unquote(text.replace('_', ' '))
     return f'C {text}'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
