c4c26740f053949eb9ab477d0272880c0ec545fe
Richard Duah Boakye <richrichie784@gmail.com>
2024-04-27 13:22:32 +0000

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -56,7 +56,7 @@ def display_number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_number_template(n):
     if isinstance(n, int):
-        return render_template('number.html', n=n)
+        return render_template('5-number.html', n=n)
     else:
         return 'Not a valid integer'
 
